c6e1ccaf0241eebab22a329522da76be9099fd5a
Nico Flaig <nflaig@protonmail.com>
2024-07-30 13:04:07 +0100

    Review PR

--- packages/beacon-node/src/api/impl/validator/index.ts
+++ packages/beacon-node/src/api/impl/validator/index.ts
@@ -1079,7 +1079,7 @@ export function getValidatorApi({
       if (!aggregate) {
         throw new ApiError(
           404,
-          `No aggregated attestation for slot=${slot} committeeIndex=${committeeIndex}, dataRoot=${dataRootHex}`
+          `No aggregated attestation for slot=${slot}, committeeIndex=${committeeIndex}, dataRoot=${dataRootHex}`
         );
       }
 
